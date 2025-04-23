1e54ae925e9bc4fb80e8aabec34b96e9987d0e7f
Flávio Augusto <100893001+flavio1508@users.noreply.github.com>
2025-04-23 12:22:03 -0300

    first commit

--- server.js
+++ server.js
@@ -9,10 +9,22 @@ const app = express();
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
-app.use(cors({
-    origin: 'http://localhost:3000',
+const allowedOrigins = [
+    'http://localhost:3000',
+    'https://stagefront.onrender.com'
+  ];
+  
+  app.use(cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
     credentials: true
-}));
+  }));
+  
 
 const db = require('./config/db'); // Importar a conexão com o banco de dados
 
