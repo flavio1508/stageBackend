a08d9ff26c2eba3aad4528b2ac98cfb7d9bd84ea
Flávio Augusto <100893001+flavio1508@users.noreply.github.com>
2025-06-02 10:54:59 -0300

    first commit

--- config/db.js
+++ config/db.js
@@ -1,11 +1,11 @@
 const { Pool } = require('pg');
+require('dotenv').config();
 
 const db = new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: 5432, // porta padrão do PostgreSQL
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, // necessário para Neon e alguns outros serviços
+  },
 });
 
 db.connect(err => {
