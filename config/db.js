36dd2e8c40881ed429aae71fb1447c53c15963e7
Flávio Augusto <100893001+flavio1508@users.noreply.github.com>
2025-06-07 11:51:32 -0300

    first commit

--- config/db.js
+++ config/db.js
@@ -1,20 +1,19 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
-const pool = new Pool({
+const db = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
-    rejectUnauthorized: false,
+    rejectUnauthorized: false, // necessário para Neon e alguns outros serviços
   },
 });
 
-pool
-  .query('SELECT NOW()') // pequeno teste para forçar conexão ao carregar
-  .then(() => {
-    console.log('Conectado ao banco de dados PostgreSQL');
-  })
-  .catch(err => {
+db.connect(err => {
+  if (err) {
     console.error('Erro ao conectar ao banco de dados PostgreSQL:', err);
-  });
+  } else {
+    console.log('Conectado ao banco de dados PostgreSQL');
+  }
+});
 
-module.exports = pool;
+module.exports = db;
